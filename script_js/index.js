caa5209e3f7413df2e68b0c3f2f8bd04466b0978
Anathyon <anathyon@hotmail.com>
2025-10-01 16:34:13 -0300

    Ajustes no texto do botão de alternância de modo (claro/escuro)

--- script_js/index.js
+++ script_js/index.js
@@ -2,6 +2,7 @@
 const html = document.documentElement;
 const avatar = document.querySelector("#avatar");
 const alternar_modo = document.querySelector("#alternar_modo");
+const modo_text = document.querySelector("#modo-text");
 // Cria os elementos do switch (botão e pino)
 const switchContainer = document.querySelector("#sp_simu");
 const switchButton = document.querySelector("#btn_modo");
@@ -9,8 +10,13 @@ const switchButton = document.querySelector("#btn_modo");
 alternar_modo.addEventListener("click", () => {
     // Alterna a classe 'claro' no <html>
     html.classList.toggle("claro");
+    if (modo_text.textContent === "Modo Aurora") {
+        modo_text.textContent = "Modo Nocturne";
+    }
+    else {
+        modo_text.textContent = "Modo Aurora";
+    }
     // Atualiza a imagem do avatar de acordo com o tema
-    // Você precisa ter 'avatar-c.svg' (claro) e 'avatar-e.svg' (escuro) na pasta 'imagens'
     if (html.classList.contains("claro")) {
         // Tema claro ativado
         avatar.setAttribute("src", "svg/avatar-a.svg"); // Mude para o nome correto da sua imagem CLARA
--- script_ts/index.ts
+++ script_ts/index.ts
@@ -1,6 +1,7 @@
 const html = document.documentElement as HTMLHtmlElement
 const avatar = document.querySelector("#avatar") as HTMLImageElement
 const alternar_modo = document.querySelector("#alternar_modo") as HTMLButtonElement
+const modo_text = document.querySelector("#modo-text") as HTMLSpanElement
 
 // Cria os elementos do switch (botão e pino)
 const switchContainer = document.querySelector("#sp_simu") as HTMLDivElement;
@@ -11,11 +12,17 @@ alternar_modo.addEventListener("click", () => {
     // Alterna a classe 'claro' no <html>
     html.classList.toggle("claro");
     
+    if (modo_text.textContent === "Modo Aurora") {
+        modo_text.textContent = "Modo Nocturne";
+    } else {
+        modo_text.textContent = "Modo Aurora";
+    }
+
     // Atualiza a imagem do avatar de acordo com o tema
-    // Você precisa ter 'avatar-c.svg' (claro) e 'avatar-e.svg' (escuro) na pasta 'imagens'
     if (html.classList.contains("claro")) {
         // Tema claro ativado
         avatar.setAttribute("src", "svg/avatar-a.svg"); // Mude para o nome correto da sua imagem CLARA
+        
     } else {
         // Tema escuro ativado
         avatar.setAttribute("src", "svg/avatar-a.svg"); // Mantenha o nome original da sua imagem ESCURA
